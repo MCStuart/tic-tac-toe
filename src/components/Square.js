b0de4895316432630da49ac4ef1bfdf5602e043f
Stuart McKay <stuart51994@gmail.com>
2020-03-31 14:26:13 -0700

    style square component

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 
 const Button = styled.button`
   background: lightblue;
+  border: 2px solid darkblue;
+  font-size: 30px;
+  font-weight: 800;
+  cursor: pointer;
+  outline: none;
 `;
 
 const Square = ({ value, onClick }) => (
