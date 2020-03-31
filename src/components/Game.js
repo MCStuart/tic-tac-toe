32d6a9f0d615e9d9685ec91883540f74c3634479
Stuart McKay <stuart51994@gmail.com>
2020-03-31 16:27:49 -0700

    rename 'board' to 'history', count turns, store game history in new array, refactor renderMoves() and define jumpTo()

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,31 +10,41 @@ const BoardText = styled.div`
 
 const Game = () => {
 
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(history[stepNumber]);
 
   const handleClick = (i) => {
-    const boardCopy = [...board];
-    if (winner || boardCopy[i]) return;
-    boardCopy[i] = xIsNext ? 'X' : 'O';
-    setBoard(boardCopy);
+    const timeInHistory = history.slice(0, stepNumber + 1);
+    const current = timeInHistory[stepNumber];
+    const squares = [...current];
+    if (winner || squares[i]) return;
+    squares[i] = xIsNext ? 'X' : 'O';
+    setHistory([...timeInHistory, squares]);
+    setStepNumber(timeInHistory.length);
     setXIsNext(!xIsNext);
   }
 
-  const jumpTo = () => {
-
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
   }
 
   const renderMoves = () => (
-    <button onClick={() => setBoard(Array(9).fill(null))}>
-      Start Game
-    </button>
+    history.map((_step, move) => {
+      const destination = move ? `Go to move#${move}` : 'Go to start';
+      return (
+        <li key={move}>
+        <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      )
+    })
   )
 
   return (
     <>
-      <Board squares={board} onClick={handleClick} />
+      <Board squares={history[stepNumber]} onClick={handleClick} />
       <BoardText>
         <p>{winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
         {renderMoves()}
