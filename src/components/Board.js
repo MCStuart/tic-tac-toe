47fc0e94eed5e2de9751953c3aa83863acbe0b9c
Stuart McKay <stuart51994@gmail.com>
2020-03-31 15:42:19 -0700

    refactor board squares to render using .map()

--- src/components/Board.js
+++ src/components/Board.js
@@ -14,15 +14,9 @@ grid-template: repeat(3, 1fr) / repeat(3, 1fr);
 
 const Board = ({ squares, onClick }) => (
   <Grid>
-    <Square value="0" onClick={() => onClick("dummy value")} />
-    <Square value="1" onClick={() => onClick("dummy value")} />
-    <Square value="2" onClick={() => onClick("dummy value")} />
-    <Square value="3" onClick={() => onClick("dummy value")} />
-    <Square value="4" onClick={() => onClick("dummy value")} />
-    <Square value="5" onClick={() => onClick("dummy value")} />
-    <Square value="6" onClick={() => onClick("dummy value")} />
-    <Square value="7" onClick={() => onClick("dummy value")} />
-    <Square value="8" onClick={() => onClick("dummy value")} />
+    {squares.map((square, i) => (
+      <Square key={i} value={square} onClick={() => onClick(i)} />
+    ))}
   </Grid>
 )
 
