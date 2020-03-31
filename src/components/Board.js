710582863cb283e6eb522cfec3260a6b78d22fdd
Stuart McKay <stuart51994@gmail.com>
2020-03-31 14:45:57 -0700

    add display grid, style

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import Square from './Square';
 
+const Grid = styled.div`
+width: 400px;
+height: 400px;
+margin: 0px;
+border: 4px solid darkblue;
+border-radius: 10px;
+display: grid;
+grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+`;
+
 const Board = ({ squares, onClick }) => (
-  <div>
+  <Grid>
     <Square value="1" onClick={() => onClick("dummy value")} />
     <Square value="2" onClick={() => onClick("dummy value")} />
     <Square value="3" onClick={() => onClick("dummy value")} />
@@ -12,7 +23,7 @@ const Board = ({ squares, onClick }) => (
     <Square value="7" onClick={() => onClick("dummy value")} />
     <Square value="8" onClick={() => onClick("dummy value")} />
     <Square value="9" onClick={() => onClick("dummy value")} />
-  </div>
+  </Grid>
 )
 
 export default Board;
